b5d9d20178bf96d9131a2b160811af5bb4ca87ad
Denis Domanin <den768653@gmail.com>
2024-05-21 14:59:44 +0300

    add routes for card and edit database.yml file

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_21_082548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string "front_side"
+    t.string "back_side"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
